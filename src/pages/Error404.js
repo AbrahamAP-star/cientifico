3ff1715cdd22b8cd005269e545cf96d75d9fb7d9
Abraham Code <fuentesabraham075@gmail.com>
2026-03-18 12:12:32 +0100

    todo nuevo

---
+++ src/pages/Error404.js
@@ -0,0 +1,12 @@
+const Error404 = () => {
+    const view = `
+    <div class="error404">
+        <h1>404</h1>
+        <p>Page not found</p>
+    </div>
+    `;
+
+    return view;
+};
+
+export default Error404;
